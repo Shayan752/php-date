21922c4b91334f6736b455256f661a9609a0bcb5
Shayan752 <37373822+Shayan752@users.noreply.github.com>
2020-04-25 23:24:09 -0700

    cms
    cms

---
+++ cms.php
@@ -0,0 +1,51 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>    
+<body>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
+
+
+    <div class="card-deck container-fluid overflow-hidden ml-5 pl-auto">
+      <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+  <div class="card-header">تعداد فرم ها</div>
+  <div class="card-body">
+    <h5 class="card-title">فرم های دریافتی</h5>
+    <h1 class="card-text">5</h1>
+    <button type="button" class="btn btn-success pl-auto ">نمایش فرم های ثبت شده</button>
+  </div>
+</div>
+<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+  <div class="card-header">فرم</div>
+  <div class="card-body">
+    <h5 class="card-title">ثبت فرم</h5>
+    <h1 class="card-text">فرم جدید</h1>
+    <button type="button" class="btn btn-warning">دریافت فرم جدید</button>
+  </div>
+</div>
+<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">پرینت فرم</div>
+  <div class="card-body">
+    <h5 class="card-title">تعداد پرینت ها </h5>
+    <h1 class="card-text">پرینت</h1>
+    <button type="button" class="btn btn-info">دریافت پرینت</button>
+  </div>
+</div>
+</div>
+</body>
+</html>
